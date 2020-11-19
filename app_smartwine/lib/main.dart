252a8b23d36186546f2b6237915e2557b48fbf60
PierreBossard <73931221+PierreBossard@users.noreply.github.com>
2020-11-19 19:21:59 +0100

    Update main.dart

--- app_smartwine/lib/main.dart
+++ app_smartwine/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:app_smartwine/screens/SignIn.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'test';
+
     return MaterialApp(
-      title: 'Flutter Demo XX',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: SignInForm(),
       ),
-      home: new SignIn();
-    );
-  }
-}
-
-
