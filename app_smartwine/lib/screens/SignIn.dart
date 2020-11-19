cf70dded815ea13f94cf6490c25ada9e8c0ab5c8
PierreBossard <73931221+PierreBossard@users.noreply.github.com>
2020-11-19 19:21:13 +0100

    Update SignIn.dart

--- app_smartwine/lib/screens/SignIn.dart
+++ app_smartwine/lib/screens/SignIn.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
+// Create a Form widget.
+class SignInForm extends StatefulWidget {
+  @override
+  SignInFormState createState() {
+    return SignInFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class SignInFormState extends State<SignInForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+=======
 
 class SignInForm extends StatefulWidget {
   @override
@@ -8,23 +26,25 @@ class SignInForm extends StatefulWidget {
 
 class _SignInFormState extends State<SignInForm> {
 
+>>>>>>> 8a5cfdfdf1261e763c94605b6daab70cadabe767
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-
           TextFormField(
             validator: (value) {
               if (value.isEmpty) {
-                return 'Please enter email';
+                return 'Please enter some name';
               }
               return null;
             },
-          );
+          ),
 
           TextFormField(
             validator: (value) {
@@ -33,22 +53,25 @@ class _SignInFormState extends State<SignInForm> {
               }
               return null;
             },
-            
-          );
+          ),
 
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState.validate()) {
-                Scaffold
-                    .of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
-            child: Text('Submit'),
-          );
-
-        ]
-     )
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
